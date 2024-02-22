8a3b23204d6473ffc7c4f17e54870f0b92e48ac4
nnnhurt <nnnzagora625@mail.ru>
2024-02-22 14:16:44 +0300

    solved tasks

---
+++ script3.sql
@@ -0,0 +1,50 @@
+-- Active: 1708455166224@@127.0.0.1@38746
+
+create EXTENSION if not exists "uuid-ossp";
+
+drop table if exists festival, shows;
+
+create table festival
+(
+    id uuid PRIMARY key DEFAULT uuid_generate_v4(),
+    name TEXT,
+    day date,
+    place text
+);
+
+
+create table shows
+(
+    id uuid PRIMARY key DEFAULT uuid_generate_v4(),
+    festival_id uuid REFERENCES festival(id),
+    name TEXT,
+    day date,
+    genre text
+);
+
+insert into festival (name, day, place)
+values
+    ('Rock-Festival', '2024.05.25', 'Krasnodar'),
+    ('Cat-Festival', '2023.11.20', 'Sochi'),
+    ('Dog-Festival', '2022.10.05', 'Moscow');
+
+insert into shows (festival_id, name, day, genre)
+VALUES
+    ((select id from festival where name='Rock-Festival'), 'Rammstein', '2024.05.25', 'rock'),
+    ((select id from festival where name='Rock-Festival'), 'Queen', '2024.05.30', 'rock'),
+    ((select id from festival where name='Cat-Festival'), 'Dansing cat', '2023.11.20', 'dance'),
+    ((select id from festival where name='Cat-Festival'), 'Singing cat', '2023.12.20', 'sing'),
+    ((select id from festival where name='Dog-Festival'), 'Playing dog', '2022.10.05', 'games'),
+    ((select id from festival where name='Dog-Festival'), 'Happy dog', '2022.11.23', 'happy');
+
+SELECT
+    f.id,
+    f.name,
+    f.day,
+    COALESCE(json_agg(json_build_object('id', s.id, 'festival_id', s.festival_id, 'name', s.name, 'day', s.day, 'genre', s.genre))
+    filter (where s.id is not null), '[]') as shows
+
+from festival f
+left join shows s on f.id=s.festival_id
+group by f.id;
+
---
+++ script5.sql
@@ -0,0 +1,51 @@
+-- Active: 1708455166224@@127.0.0.1@38746
+
+create EXTENSION if not exists "uuid-ossp";
+
+drop table if exists repository, tikets;
+
+create table repository
+(
+    id uuid PRIMARY key DEFAULT uuid_generate_v4(),
+    name TEXT,
+    description text,
+    stars int
+);
+
+
+create table tikets
+(
+    id uuid PRIMARY key DEFAULT uuid_generate_v4(),
+    repository_id uuid REFERENCES repository(id),
+    name text,
+    description text,
+    status text
+);
+
+insert into repository (name, description, stars)
+values
+    ('First_repos', 'cool', '5'),
+    ('Second_repos', 'well done', '4'),
+    ('Third_repos', 'horror', '2');
+
+insert into tikets (repository_id, name, description, status)
+VALUES
+    ((select id from repository where name='First_repos'), 'Cat', 'beautiful', 'OK'),
+    ((select id from repository where name='First_repos'), 'Dog', 'cool', 'OK'),
+    ((select id from repository where name='Second_repos'), 'Rabbit', 'klass', 'OK'),
+    ((select id from repository where name='Second_repos'), 'Bird', 'super', 'OK'),
+    ((select id from repository where name='Third_repos'), 'Rat', 'bad', 'BAD'),
+    ((select id from repository where name='Third_repos'), 'Mouse', 'very bad', 'BAD');
+
+SELECT
+    r.id,
+    r.name,
+    r.description,
+    r.stars,
+    COALESCE(json_agg(json_build_object('id', t.id, 'repository_id', t.repository_id, 'name', t.name, 'description', t.description, 'status', t.status))
+    filter (where t.id is not null), '[]') as tikets
+
+from repository r
+left join tikets t on r.id=t.repository_id
+group by r.id;
+
